f103bb64df79be13464c2e7609aef8edfa2673d9
Sahil <sahil3m3@gmail.com>
2023-11-03 16:00:16 +0530

    Assignment-Q3-done

---
+++ Assignment-2/Q1/src/SelectionSort.java
@@ -0,0 +1,32 @@
+
+public class SelectionSort {
+	static public int selSort(int a[],int size)
+	{
+		int count=0;
+		for(int j=0;j<size;j++)
+		{
+		for(int i=0;i<size;i++)
+		{count++;
+			if(a[j]<a[i])
+			{
+				int temp=a[j];
+				a[j]=a[i];
+				a[i]=temp;
+			
+			}
+		}
+		}
+		return count;
+	}//selSort()
+	public static void main(String[] args) {
+		int a[]= {44,11,55,22,66,33};
+		for(int i=0;i<a.length;i++)
+			System.out.print(" "+a[i]);
+		int count =selSort(a,a.length);
+		System.out.println("\n After sort ("+count +") no comparisions");
+		for(int i=0;i<a.length;i++)
+			System.out.print(" "+a[i]);
+
+	}
+
+}
---
+++ Assignment-2/Q2/src/BubbleSort.java
@@ -0,0 +1,34 @@
+
+public class BubbleSort {
+static public int bubbleSort(int a[],int size)
+{
+	int count=0;
+	for(int j=0;j<size;j++)
+	{
+	for(int i=0;i<size-1;i++)
+	{
+		count++;
+		if(a[i]>a[i+1])
+		{
+			int t=a[i];
+			a[i]=a[i+1];
+			a[i+1]=t;
+		}
+	}
+	}
+	return count;
+}
+	public static void main(String[] args) {
+		int arr[] = {33, 22, 66, 55, 44, 11};
+		for(int i=0;i<arr.length;i++)
+			System.out.print(" "+arr[i]);
+		
+		int c=bubbleSort(arr,arr.length);
+		System.out.println("\n After Sort ("+c+") no of comparsion");
+		for(int i=0;i<arr.length;i++)
+			System.out.print(" "+arr[i]);
+		
+
+	}
+
+}
